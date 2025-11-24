0cc88061180663ee05b54d9185d69ca190c3ebf5
Abhishek N <abhishekpes123@gmail.com>
2025-11-24 13:04:48 +0530

    first-commit

--- src/Repositories/Roles/role.ts
+++ src/Repositories/Roles/role.ts
@@ -1,4 +1,7 @@
+import { Module } from "../../Models/module.model";
 import { Role } from "../../Models/user.role.model";
+import { UserModules } from "../../Models/user_modules.model";
+import { IModule } from "../../Types/Module";
 import { UserRole } from "../../Types/Role";
 
 export class Rolefunctions {
@@ -18,7 +21,33 @@ export class Rolefunctions {
     static async fetchByNames(name: string) {
         return await Role.findOne({ name });
     }
+   static async fetchRolesWithModules(): Promise<(UserRole & { modules: IModule[] })[]> {
+  const roles = await Role.find(); // get all roles
 
+  const rolesWithModules = await Promise.all(
+    roles.map(async (role) => {
+      // Find user_modules entry for this role
+      const userModulesEntry = await UserModules.findOne({
+        user_group_id: role._id,
+      });
+
+      let modules: IModule[] = []; // explicitly type it
+      if (userModulesEntry && userModulesEntry.module_id.length) {
+        // Fetch all modules whose _id is in module_id array
+        modules = await Module.find({
+          _id: { $in: userModulesEntry.module_id },
+        }) as IModule[]; // type cast
+      }
+
+      return {
+        ...role.toObject(),
+        modules, // add modules array to the role
+      };
+    })
+  );
+
+  return rolesWithModules;
+}
     static async fetchRoles() {
         return await Role.find();
     }
