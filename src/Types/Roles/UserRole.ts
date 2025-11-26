8215ed90b7595a42d40ab0dd8024acf020ff0afc
Abhishek N <abhishekpes123@gmail.com>
2025-11-26 13:13:18 +0530

    chnages in userole type

--- src/Types/Roles/UserRole.ts
+++ src/Types/Roles/UserRole.ts
@@ -3,7 +3,7 @@ import { Types } from "mongoose";
 import { IUserModulesResponse } from "../UserModule/userModuleResponse";
 
 
-export interface UserRole extends Document {
+export interface UserRole {
   _id: Types.ObjectId;
   name: string;
   modules: IUserModulesResponse[]; // each role has an array of IUserModules
